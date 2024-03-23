f0faca1c8f4e6883c0ce547849e7e67e88f0e660
Dawson <dawson9jordan@gmail.com>
2024-03-23 13:29:40 -0600

    implemented blinking

--- key_controller.py
+++ key_controller.py
@@ -1,9 +1,11 @@
 from tkinter import Tk, Label
 from tango import Tango
+from proj3_threads import Screen
 import time
 
 def key_pressed(event):
     pressed = event.char
+    display = Screen()
 
     # Movement variables
     ROTATION = 200
@@ -12,90 +14,108 @@ def key_pressed(event):
 
     # WHEELS
     if(pressed == 'w'):
-        w=Label(root,text="Moving forward")
+        display.move()
+        #w=Label(root,text="Moving forward")
         t.setServo(0, 6000 - (SPEED * ROTATION * 4))
         time.sleep(1)
         t.reset(0)
-
     elif(pressed == 's'):
-        w=Label(root,text="Moving backwards")
+        display.move()
+        #w=Label(root,text="Moving backwards")
         t.setServo(0, 6000 + (SPEED * ROTATION * 4))
         time.sleep(1)
         t.reset(0)
     elif(pressed == '='):
-        w=Label(root,text="Incrementing speed")
+        #w=Label(root,text="Incrementing speed")
         SPEED = SPEED + 1
         if(SPEED == 4):
             SPEED = 1
-        
     elif(pressed == 'a'):
-        w=Label(root,text="Turning left")
+        display.move()
+        #w=Label(root,text="Turning left")
         t.setServo(1, 6000 + (SPEED * ROTATION * 4))
         time.sleep(1)
         t.reset(1)
     elif(pressed == 'd'):
-        w=Label(root,text="Turning right")
+        display.move()
+        #w=Label(root,text="Turning right")
         t.setServo(1, 6000 - (SPEED * ROTATION * 4))
         time.sleep(1)
         t.reset(1)
     elif(pressed == '-'):
-        w=Label(root,text="Changing direction")
+        #w=Label(root,text="Changing direction")
         DIRECTION *= -1
 
     # HEAD & WAIST
     elif(pressed == 'z'):
-        w=Label(root,text="Moving waist")
+        display.drawEyes(0)
+        #w=Label(root,text="Moving waist")
         t.moveServo(2, ROTATION * DIRECTION)
     elif(pressed == ','):
-        w=Label(root,text="Turning head")
+        display.drawEyes(0)
+        #w=Label(root,text="Turning head")
         t.moveServo(3, ROTATION * DIRECTION)
     elif(pressed == '.'):
-        w=Label(root,text="Tilting head")
+        display.drawEyes(0)
+        #w=Label(root,text="Tilting head")
         t.moveServo(4, ROTATION * DIRECTION)
 
     # RIGHT ARM
     elif(pressed == 't'):
-        w=Label(root,text="Moving right shoulder")
+        display.drawEyes(0)
+        #w=Label(root,text="Moving right shoulder")
         t.moveServo(5, ROTATION * DIRECTION)
     elif(pressed == 'y'):
-        w=Label(root,text="Moving right bicep")
+        display.drawEyes(0)
+        #w=Label(root,text="Moving right bicep")
         t.moveServo(6, ROTATION * DIRECTION)
     elif(pressed == 'u'):
-        w=Label(root,text="Moving right elbow")
+        display.drawEyes(0)
+        #w=Label(root,text="Moving right elbow")
         t.moveServo(7, ROTATION * DIRECTION)
     elif(pressed == 'i'):
-        w=Label(root,text="Moving right forearm")
+        display.drawEyes(0)
+        #w=Label(root,text="Moving right forearm")
         t.moveServo(8, ROTATION * DIRECTION)
     elif(pressed == 'o'):
-        w=Label(root,text="Moving right wrist")
+        display.drawEyes(0)
+        #w=Label(root,text="Moving right wrist")
         t.moveServo(9, ROTATION * DIRECTION)
     elif(pressed == 'p'):
-        w=Label(root,text="Moving right gripper")
+        display.drawEyes(0)
+        #w=Label(root,text="Moving right gripper")
         t.moveServo(10, ROTATION * DIRECTION)
 
     # LEFT ARM
     elif(pressed == 'f'):
-        w=Label(root,text="Moving left shoulder")
+        display.drawEyes(0)
+        #w=Label(root,text="Moving left shoulder")
         t.moveServo(11, ROTATION * DIRECTION)
     elif(pressed == 'g'):
-        w=Label(root,text="Moving left bicep")
+        display.drawEyes(0)
+        #w=Label(root,text="Moving left bicep")
         t.moveServo(12, ROTATION * DIRECTION)
     elif(pressed == 'h'):
-        w=Label(root,text="Moving left elbow")
+        display.drawEyes(0)
+        #w=Label(root,text="Moving left elbow")
         t.moveServo(13, ROTATION * DIRECTION)
     elif(pressed == 'j'):
-        w=Label(root,text="Moving left forearm")
+        display.drawEyes(0)
+        #w=Label(root,text="Moving left forearm")
         t.moveServo(14, ROTATION * DIRECTION)
     elif(pressed == 'k'):
-        w=Label(root,text="Moving left wrist")
+        display.drawEyes(0)
+        #w=Label(root,text="Moving left wrist")
         t.moveServo(15, ROTATION * DIRECTION)
     elif(pressed == 'l'):
-        w=Label(root,text="Moving left gripper")
+        display.drawEyes(0)
+        #w=Label(root,text="Moving left gripper")
         t.moveServo(16, ROTATION * DIRECTION)
     else:
-        w=Label(root,text="Key Pressed: "+ pressed)
+        display.drawEyes(0)
+        #w=Label(root,text="Key Pressed: "+ pressed)
 
-    w.place(x=70,y=90)
+    #w.place(x=70,y=90)
 
 if __name__ == "__main__":
     root=Tk()
--- proj3_threads.py
+++ proj3_threads.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from threading import Thread
+import random
+import threading
 from speech import TTS
 
 class Screen:
@@ -11,6 +12,8 @@ class Screen:
         self.movePupilsId = None
         self.moveFigureId = None
         self.spiralId = None
+        self.blinkId  = None
+        self.nextBlinkId = None
         self.speech = TTS()
 
         self.drawEyes(0)
@@ -25,6 +28,12 @@ class Screen:
         if self.spiralId is not None:
             self.canvas.after_cancel(self.spiralId)
             self.spiralId = None
+        if self.blinkId is not None:
+            self.canvas.after_cancel(self.blinkId)
+            self.blinkId = None
+        if self.nextBlinkId is not None:
+            self.canvas.after_cancel(self.nextBlinkId)
+            self.nextBlinkId = None
         self.canvas.delete("all")
     
     def drawEyes(self, look):
@@ -32,7 +41,7 @@ class Screen:
         screenHeight = self.canvas.winfo_screenheight()
         screenWidth = self.canvas.winfo_screenwidth()
         margin = 15
-        face = self.canvas.create_oval(margin, margin, screenWidth - margin, screenHeight - margin, outline = "black", width = 5)
+        face = self.canvas.create_oval(margin, margin, screenWidth - margin, screenHeight - margin, outline = "black", width = 5, tags = "face")
 
         eyeRadius = screenHeight/4
         leftEye = self.canvas.create_oval(screenWidth/2 - eyeRadius*2, screenHeight/2 - eyeRadius, screenWidth/2, screenHeight/2 + eyeRadius, outline = "black", fill = "white", tags = "leftEye")
@@ -41,22 +50,50 @@ class Screen:
         pupilRadius = eyeRadius/2
         leftPupil = self.canvas.create_oval(screenWidth/2 - eyeRadius - pupilRadius, screenHeight/2 - eyeRadius + margin, screenWidth/2 - eyeRadius + pupilRadius, screenHeight/2 + eyeRadius - margin, outline = "black", fill = "black", tags = "leftPupil")
         rightPupil = self.canvas.create_oval(screenWidth/2 + eyeRadius - pupilRadius, screenHeight/2 - eyeRadius + margin, screenWidth/2 + eyeRadius + pupilRadius, screenHeight/2 + eyeRadius - margin, outline = "black", fill = "black", tags = "rightPupil")
-        
+
+        x1, y1, x2, y2 = self.canvas.bbox("face")
+        topLid = self.canvas.create_arc(x1+margin, y1+margin, x2-margin, y2-margin, start=0, extent=180, outline="#dcdcdc", fill="#dcdcdc", tags="topLid")
+        botLid = self.canvas.create_arc(x1+margin, y1+margin, x2-margin, y2-margin, start=0, extent=-180, outline="#dcdcdc", fill="#dcdcdc", tags="botLid")
+        self.canvas.move("topLid", 0, -(self.canvas.winfo_screenheight() / 2))
+        self.canvas.move("botLid", 0, self.canvas.winfo_screenheight() / 2)
+
+        self.canvas.tag_raise("face")
+
+        self.nextBlink()
+
         if(look == 0): # move
-            self.movePupils(1, 1)
+            self.movePupils(0.5, 0.5)
         elif(look == 1): # up
-            self.canvas.move("leftPupil", 0, (-2/3)*eyeRadius)
-            self.canvas.move("rightPupil", 0, (-2/3)*eyeRadius)
+            self.canvas.move("leftPupil", 0, (-1/3)*eyeRadius)
+            self.canvas.move("rightPupil", 0, (-1/3)*eyeRadius)
         elif(look == 2): # right
             self.canvas.move("leftPupil", (2/3)*eyeRadius, 0)
             self.canvas.move("rightPupil", (2/3)*eyeRadius, 0)
         elif(look == 3): # down
-            self.canvas.move("leftPupil", 0, (2/3)*eyeRadius)
-            self.canvas.move("rightPupil", 0, (2/3)*eyeRadius)
+            self.canvas.move("leftPupil", 0, (1/3)*eyeRadius)
+            self.canvas.move("rightPupil", 0, (1/3)*eyeRadius)
         elif(look == 4): # left
             self.canvas.move("leftPupil", (-2/3)*eyeRadius, 0)
             self.canvas.move("rightPupil", (-2/3)*eyeRadius, 0)
-    
+
+    def nextBlink(self):
+        t = random.randint(1000, 4000)
+        self.nextBlinkId = self.canvas.after(t, lambda: self.blink(30))
+
+    def blink(self, velo):
+        self.canvas.move("topLid", 0, velo)
+        self.canvas.move("botLid", 0, -velo)
+        _, _, _, y2 = self.canvas.bbox("topLid")
+        _, y1, _, _ = self.canvas.bbox("botLid")
+
+        if y2 >= y1:
+            velo *= -1
+        elif y2 <= 0:
+            self.nextBlink()
+            return
+
+        self.blinkId = self.canvas.after(10, lambda: self.blink(velo))
+
     def movePupils(self, xVelo, yVelo):
         self.canvas.move("leftPupil", xVelo, yVelo)
         self.canvas.move("rightPupil", xVelo, yVelo)
@@ -98,7 +135,7 @@ class Screen:
         self.spiral(0, fontSize, fontSize)
 
         if speak:
-            Thread(target=self.speech.speak, args=(word,)).start()
+            threading.Thread(target=self.speech.speak, args=(word,)).start()
 
     def spiral(self, angle, initialSize, currentSize):
         self.canvas.itemconfig("word", angle=angle, font=("Helvetica", currentSize, "bold"))
@@ -112,7 +149,7 @@ class Screen:
         self.canvas.create_text(self.canvas.winfo_screenwidth()//2, self.canvas.winfo_screenheight()//2, text=textContent, font=("Helvetica", fontSize), fill="black")
         
         if speak:
-            Thread(target=self.speech.speak, args=(textContent,)).start()
+            threading.Thread(target=self.speech.speak, args=(textContent,)).start()
 
 if __name__ == "__main__":
     display = Screen()
