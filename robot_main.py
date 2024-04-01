76dfa5d5930caa76890b2d79ab4270fc8e619452
Dawson <dawson9jordan@gmail.com>
2024-04-01 13:03:24 -0600

    remove thread

--- robot_main.py
+++ robot_main.py
@@ -7,5 +7,5 @@ import tkinter as tk
 if __name__ == "__main__":  
     root=tk.Tk()
     Thread(target=screen.run, args=(root,)).start() # start screen in seperate thread
-    Thread(target=key_controller.run, args=(root,)).start() # start key controller
+    target=key_controller.run(root) # start key controller
     root.mainloop()
