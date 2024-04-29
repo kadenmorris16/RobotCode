ab8e32b94354ee880ad8bee8fffaa288d38856f6
Dawson <dawson9jordan@gmail.com>
2024-04-29 12:32:05 -0600

    test

--- gui.py
+++ gui.py
@@ -262,6 +262,7 @@ class RobotProgrammingGUI:
         apply_button = tk.Button(popup_window, text="✅", command=lambda: self.apply_adjustments(icon_name, popup_window), font=("Arial", font_size), padx=10, pady=5, borderwidth=2, relief=tk.RAISED, background="lightgray")
         apply_button.pack(pady=(font_size,font_size))
         #popup_window.grab_set()
+        popup_window.focus_set()
 
     def apply_adjustments(self, icon_name, popup_window):
         string = ""
