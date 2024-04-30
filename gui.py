7cd662e8f5ec5417d164118c5d7c006154c6d5f5
Dawson <dawson9jordan@gmail.com>
2024-04-29 21:17:02 -0600

    test

--- gui.py
+++ gui.py
@@ -320,7 +320,7 @@ class RobotProgrammingGUI:
 
     def open_keyboard(self, event):
         # Open the system's default on-screen keyboard
-        subprocess.Popen(['gkbd-keyboard-display'])
+        subprocess.Popen(['xdotool', 'search', '--onlyvisible', '--class', 'osk', 'windowactivate', '--sync', 'key', '--clearmodifiers', 'shift+F10'])
 
     def play_timeline(self):
         #RobotProgrammingParser(self.actions)
